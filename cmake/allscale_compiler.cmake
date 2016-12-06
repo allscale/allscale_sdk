4d1106230295200905f872cd0b1432336d01c0e3
Alex Hirsch <alexander.hirsch@uibk.ac.at>
2016-12-06 15:50:35 +0100

    SDK: Pass INSIEME_LIBS_HOME to allscale_compiler

--- cmake/allscale_compiler.cmake
+++ cmake/allscale_compiler.cmake
@@ -4,7 +4,11 @@ if(NOT TARGET allscale_compiler)
 	ExternalProject_Add(
 		allscale_compiler
 		GIT_REPOSITORY git@goedis:philipp.gschwandtner/allscale-compiler.git
-		BUILD_COMMAND make allscalecc
+		UPDATE_COMMAND ""
+		CMAKE_COMMAND
+			${CMAKE_COMMAND} -E env
+			"INSIEME_LIBS_HOME=${PROJECT_SOURCE_DIR}/third_party"
+			${CMAKE_COMMAND}
 		INSTALL_COMMAND ""
 		TEST_COMMAND ""
 		DOWNLOAD_NO_PROGRESS 1
